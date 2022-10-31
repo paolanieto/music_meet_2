a5f856d8c4bd57639914164963ccc79b56e1c415
John <christian.vega.720@my.csun.edu>
2022-10-31 11:00:32 -0700

    FInally Got The Edit Working

--- base/views.py
+++ base/views.py
@@ -8,6 +8,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from .models import Event, Topic, Message, Musician, Group
 from .forms import EventForm, UserForm, MusicianForm, GroupForm
+from django.core.exceptions import ObjectDoesNotExist
 # Create your views here.
 
 #events = [
@@ -272,7 +273,20 @@ def deleteMessage(request, pk):
 @login_required(login_url='login')
 def updateUser(request):
     user = request.user
+    
     form = UserForm(instance=user)
+    try:
+        musician = Musician.objects.get(user=user)
+    except ObjectDoesNotExist:
+        musician = None 
+    try:
+        group = Group.objects.get(user=user)
+    except ObjectDoesNotExist:
+        group = None
+
+
+    #musician = Musician.objects.get(user=user)
+    
     
     #musician = request.POST.get('musician')
     #group = request.POST.get('group')
@@ -284,6 +298,6 @@ def updateUser(request):
             form.save()
             return redirect('user-profile', pk=user.id)
     
-    context = {'form': form}
+    context = {'form': form, 'musician': musician, 'group': group}
     return render(request, 'base/update_user.html', context)
 
