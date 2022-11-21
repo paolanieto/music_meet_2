474f1c4b42908ed1888dab16394f945adebbde91
John <christian.vega.720@my.csun.edu>
2022-11-21 13:40:26 -0800

    made some flier changes

--- base/forms.py
+++ base/forms.py
@@ -1,10 +1,12 @@
 from django.forms import ModelForm
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import Event, Group, Musician, User
 #from django.contrib.auth.models import User
 
 
 class MyUserCreationForm(UserCreationForm):
+    musician_Account = UserCreationForm.BooleanField(widget=UserCreationForm.Select(choices=(True, False)))
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'musician_Account', 'group_Account', 'username', 'email', 'password1', 'password2']
